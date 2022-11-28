b3880b00dd08a48774758b060873e0120ea6ef02
Roch Blonndiaux <contact@roch-blondiaux.com>
2022-11-28 17:14:51 +0100

    Legacy Colors parsing

--- MMOItems-API/src/main/java/net/Indyuce/mmoitems/util/AdventureUtils.java
+++ MMOItems-API/src/main/java/net/Indyuce/mmoitems/util/AdventureUtils.java
@@ -9,9 +9,17 @@ import io.lumine.mythic.lib.adventure.text.minimessage.tag.resolver.ArgumentQueu
 import io.lumine.mythic.lib.adventure.text.minimessage.tag.resolver.TagResolver;
 import io.lumine.mythic.lib.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import io.lumine.mythic.lib.parser.client.eval.DoubleEvaluator;
+import net.Indyuce.mmoitems.MMOItems;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.UnaryOperator;
 
 /**
  * mmoitems
@@ -24,17 +32,29 @@ public class AdventureUtils {
     private static final MiniMessage MINI_MESSAGE;
 
     static {
+        Map<Character, String> LEGACY_COLORS = new HashMap<>(Map.of('0', "black", '1', "dark_blue", '2', "dark_green", '3', "dark_aqua", '4', "dark_red", '5', "dark_purple", '6', "gold", '7', "gray", '8', "dark_gray", '9', "blue"));
+        LEGACY_COLORS.putAll(Map.of('a', "green", 'b', "aqua", 'c', "red", 'd', "light_purple", 'e', "yellow", 'f', "white"));
+        LEGACY_COLORS.putAll(Map.of('k', "obfuscated", 'l', "bold", 'm', "strikethrough", 'n', "underline", 'o', "italic", 'r', "reset"));
+        final UnaryOperator<String> LEGACY_TRANSLATOR = s -> {
+            for (Map.Entry<Character, String> entry : LEGACY_COLORS.entrySet())
+                s = s.replace("&%c".formatted(entry.getKey()), "<%s>".formatted(entry.getValue()))
+                        .replace("%c%c".formatted(ChatColor.COLOR_CHAR, entry.getKey()), "<%s>".formatted(entry.getValue()));
+            return s;
+        };
+
+        final BiFunction<ArgumentQueue, Context, Tag> MATH_TAG = (argumentQueue, context) -> {
+            final String expression = argumentQueue.popOr("The <math> tag requires exactly one argument, the formula to apply.").value();
+            final String result = MythicLib.plugin.getMMOConfig().decimals.format(new DoubleEvaluator().evaluate(expression));
+
+            return Tag.inserting(Component.text(result));
+        };
+
         MINI_MESSAGE = MiniMessage.builder()
-                .editTags(builder -> builder.resolver(TagResolver.resolver("math", AdventureUtils::createMathTag)))
+                .editTags(builder -> builder.resolver(TagResolver.resolver("math", MATH_TAG)))
+                .preProcessor(LEGACY_TRANSLATOR)
                 .build();
     }
 
-    private static Tag createMathTag(final ArgumentQueue args, final Context ctx) {
-        final String expression = args.popOr("The <math> tag requires exactly one argument, the formula to apply.").value();
-        final String result = MythicLib.plugin.getMMOConfig().decimals.format(new DoubleEvaluator().evaluate(expression));
-
-        return Tag.inserting(Component.text(result));
-    }
 
     /**
      * Convert a component to a legacy string
@@ -76,5 +96,19 @@ public class AdventureUtils {
 
     public static @NotNull Collection<Component> miniMessage(@NotNull Collection<String> strings, TagResolver... resolvers) {
         return strings.stream().map(string -> miniMessage(string, resolvers)).toList();
+    }
+
+    public static void message(@NotNull Player player, @NotNull Component component) {
+        MMOItems.ADVENTURE.player(player).sendMessage(component);
+    }
+
+    public static @NotNull ItemMeta lore(@NotNull ItemMeta meta, @NotNull Collection<Component> lore) {
+        meta.setLore(lore.stream().map(AdventureUtils::legacyComponent).toList());
+        return meta;
+    }
+
+    public static @NotNull ItemMeta displayName(@NotNull ItemMeta meta, @NotNull Component displayName) {
+        meta.setDisplayName(legacyComponent(displayName));
+        return meta;
     }
 }
